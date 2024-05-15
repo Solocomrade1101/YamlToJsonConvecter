f376891d734af496d8b6a79a164fb60ecead42c0
Solocomrade1101 <solocomrade@mail.ru>
2024-05-15 18:21:34 +0300

    add welcome page

--- dist/js/background.js
+++ dist/js/background.js
@@ -1,3 +1,22 @@
+
+chrome.runtime.onInstalled.addListener(async () => {
+  let url = chrome.runtime.getURL("html/hello.html");
+  let tab = await chrome.tabs.create({ url });
+
+  chrome.storage.sync.get(['showClock'], (result) => {
+    if (result.showClock) {
+      chrome.action.setBadgeText({ text: 'ON' });
+    }
+  });
+
+  chrome.storage.sync.get(['timer'], (result) => {
+    console.log('result', result)
+    if (!result.timer) {
+      chrome.storage.sync.set({ 'timer': 1 })
+    }
+  });
+});
+
 chrome.commands.onCommand.addListener( (command) => {
 
   chrome.tabs.update({}, async (tab) => {
